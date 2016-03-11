dce71555ac20c3fb4c456ec365520442ea034f3d
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2016-03-10 23:44:52 -0300

    Remove sendgrid config.

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'henriquecdf',
-  :password => 'hc031190',
-  :domain => 'example.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
